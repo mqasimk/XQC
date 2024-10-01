55a104fb4c371f24206186f398872fb1d2ea5e1a
mqasimk <mqasimk.96@gmail.com>
2024-10-01 13:00:04 -0400

    Jitted classes have to be pure. Therefore the isHerm and pos attributes have been discarded as the constructor cannot have conditionals in the traditional sense. Multiplication with scalars of the Op class has been implemented and the Op initializer casts the input to jnp.complex64 by default.

--- xqc/baseops.py
+++ xqc/baseops.py
@@ -9,68 +9,91 @@ from typeguard import typechecked
 class Op:
     @typechecked
     def __init__(self, arr: jnp.ndarray):
-        if (arr.ndim != 2 or arr.shape[0] != arr.shape[1]):
+        if arr.ndim != 2 or arr.shape[0] != arr.shape[1]:
             raise ValueError("The input array must be a square jax.numpy array")
-        self.operator = jnp.array(arr)
-        if ((arr.conj()).transpose() == arr).all:
-            self.isHerm = True
-        else:
-            self.isHerm = False
-        if self.isHerm:
-            if (jnp.linalg.eigh(arr)[0] >= 0).all:
-                self.pos = True
-            else:
-                self.pos = False
+        self.operator = jnp.array(arr, dtype=jnp.complex64)
         self.shape = arr.shape
     def __str__(self):
-        return f"Op[\nvalue=\n{self.operator},\n Hermitian={self.isHerm},\n Positive={self.pos},\nshape={self.shape}\n]"
+        return f"Op[\nvalue=\n{self.operator},\nshape={self.shape}\n]"
     def __repr__(self):
         return self.__str__()
-
-
     @jax.jit
     def __matmul__(self, other):
         if isinstance(other, Op):
             return Op(self.operator @ other.operator)
         return NotImplemented
-
-
+    @jax.jit
+    def __mul__(self, other):
+        other = jnp.complex64(other)
+        if other.dtype in [jnp.int32, jnp.int64, jnp.float32, jnp.float64, jnp.complex64]:
+            return Op(self.operator * other)
+        return NotImplemented
+    @jax.jit
+    def __rmul__(self, other):
+        other = jnp.complex64(other)
+        if other.dtype in [jnp.int32, jnp.int64, jnp.float32, jnp.float64, jnp.complex64]:
+            return Op(self.operator * other)
+        return NotImplemented
     @jax.jit
     def __add__(self, other):
         if isinstance(other, Op):
             return Op(self.operator + other.operator)
         return NotImplemented
-
-
     @jax.jit
     def __sub__(self, other):
         if isinstance(other, Op):
             return Op(self.operator - other.operator)
         return NotImplemented
-
-
     @jax.jit
     def eigs(self):
-        if self.isHerm:
-            return jnp.linalg.eigh(self.operator)
-        else:
-            raise ValueError("The Op is not Hermitian and may not have a real spectrum")
-
-
+        jnp.linalg.eigh(self.operator)
     def tree_flatten(self):
-        return ((self.operator,), (self.isHerm, self.pos, self.shape))
-
-
+        return (self.operator,), (self.shape,)
+    @jax.jit
+    def T(self):
+        return Op(self.operator.transpose())
+    @jax.jit
+    def conj(self):
+        return Op(self.operator.conj())
+    @jax.jit
+    def dag(self):
+        return Op(self.operator.conj().transpose())
     @classmethod
     def tree_unflatten(cls, aux_data, children):
         return cls(*children)
 
 
 @jax.jit
-def tensor(op1, op2):
+def tensor(op1: Op, op2: Op) -> Op:
     #implementation of the same function as qutip has
     if isinstance(op1, Op) and isinstance(op2, Op):
         return Op(jnp.kron(op1.operator, op2.operator))
     else:
         raise TypeError
+
+
+@jax.jit
+def comm(op1: Op, op2: Op) -> Op:
+    return op1@op2-op2@op1
+
+
+@jax.jit
+def acomm(op1: Op, op2: Op) -> Op:
+    return op1@op2+op2@op1
+
+
+@jax.jit
+def sx():
+    return Op(jnp.array([[0, 1],[1, 0]]))
+
+
+@jax.jit
+def sy():
+    return Op(jnp.array([[0, -1j],[1j, 0]]))
+
+
+@jax.jit
+def sz():
+    return Op(jnp.array([[1, 0],[0, -1]]))
+
 
